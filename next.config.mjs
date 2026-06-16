b19b75fc6af9c161c09e60cd4d847e51ef392d39
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-16 22:16:36 +0500

    Configure static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+  basePath: isGithubActions ? '/portfolio-v4' : '',
+  assetPrefix: isGithubActions ? '/portfolio-v4/' : '',
+};
+
 export default nextConfig;
